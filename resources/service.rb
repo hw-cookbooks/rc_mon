3335287cb888b78debf9d3e9dd7bb7ac56de8a1b
Chris Roberts <chrisroberts.code@gmail.com>
2013-05-21 07:56:11 -0700

    Fix how we grab variable. Update method_missing to behave as expected

--- resources/service.rb
+++ resources/service.rb
@@ -1,4 +1,4 @@
-RUNIT_ACTIONS = Chef::Resource::RunitService.new('rc_mon').instance_variable_get(@:allowed_actions).map(&:to_sym)
+RUNIT_ACTIONS = Chef::Resource::RunitService.new('rc_mon').instance_variable_get(:@allowed_actions).map(&:to_sym)
 
 default_action :enable
 actions *RUNIT_ACTIONS
@@ -11,7 +11,11 @@ attribute :memory_limit, :kind_of => [String,Numeric]
 attribute :swap_limit, :kind_of => [String,Numeric]
 attribute :cpu_shares, :kind_of => Numeric
 
-def method_missing(k,v)
-  @runit_attributes ||= Mash.new
-  @runit_attributes[k] = v
+def method_missing(*args)
+  if(args.size != 2 || args.first.to_s.end_with?('='))
+    super
+  else
+    @runit_attributes ||= Mash.new
+    @runit_attributes[args.first] = args.last
+  end
 end
