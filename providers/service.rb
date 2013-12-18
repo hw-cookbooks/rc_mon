089ca5c58f179d8f4ce4e674b3c189110c7f1197
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-17 21:41:25 -0800

    Ensure we load the default recipe when LWRPs are being used

--- providers/service.rb
+++ providers/service.rb
@@ -2,6 +2,7 @@ include ::RcMon::ProviderMethods
 
 def load_current_resource
   new_resource.group_name new_resource.name unless new_resource.group_name
+  run_context.include_recipe 'rc_mon'
 end
 
 RUNIT_ACTIONS = Chef::Resource::RunitService.new('rc_mon').instance_variable_get(:@allowed_actions).each do |action_name|
